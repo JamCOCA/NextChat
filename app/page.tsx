3a7f2f6d2ccf4f649ca465891e95ff17da3c6c32
JamCOCA <617542730@qq.com>
2026-01-09 10:29:31 +0800

    增加备案号

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
 import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/home";
 import { getServerSideConfig } from "./config/server";
+import styles from "./page.module.scss";
 
 const serverConfig = getServerSideConfig();
 
 export default async function App() {
+  const icpNumber = process.env.NEXT_PUBLIC_ICP_NUMBER;
+  const icpLink =
+    process.env.NEXT_PUBLIC_ICP_LINK ?? "https://beian.miit.gov.cn/";
+
   return (
-    <>
+    <div className={styles.page}>
       <Home />
-      {serverConfig?.isVercel && (
-        <>
-          <Analytics />
-        </>
-      )}
-    </>
+
+      <footer className={styles.footer}>
+        <a href={icpLink} target="_blank" rel="noreferrer">
+          {icpNumber
+            ? `ICP备案号：${icpNumber}`
+            : "ICP备案号：请配置 NEXT_PUBLIC_ICP_NUMBER"}
+        </a>
+      </footer>
+
+      {serverConfig?.isVercel && <Analytics />}
+    </div>
   );
 }
